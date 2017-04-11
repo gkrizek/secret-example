68ab4109267b98122d874f7555745a28e01288cd
Graham Krizek <gkrizek@nodesource.com>
2017-04-11 15:41:59 -0600

    Got envs working

--- env.js
+++ env.js
@@ -2,24 +2,30 @@ const AWS = require('aws-sdk')
 AWS.config.update({region:'us-west-2'})
 const ssm = new AWS.SSM()
 
-const env = (!process.env.NS_ENV) ? dev : process.env.NS_ENV
+const env = (!process.env.NS_ENV) ? 'dev' : process.env.NS_ENV
 
-var params = {
-  Names: [
-    `${env}.test.API_TOKEN`,
-    `${env}.test.PASSWORD`,
-    `${env}.test.DOMAIN`
-  ],
-  WithDecryption: true
-}
 
-ssm.getParameters(params, (err, data) => {
-  if (err) {
-    console.log(err, err.stack)
-    process.exit(1)
-  } else {
-    process.env['API_TOKEN'] = data.Parameters[0].Value
-    process.env['PASSWORD'] = data.Parameters[1].Value
-    process.env['DOMAIN'] = data.Parameters[2].Value
+module.exports = {
+  variables: function(cb){
+    var params = {
+      Names: [
+        `${env}.test.API_TOKEN`,
+        `${env}.test.DOMAIN`,
+        `${env}.test.PASSWORD`
+      ],
+      WithDecryption: true
+    }
+
+    ssm.getParameters(params, (err, data) => {
+      if (err) {
+        console.log(err, err.stack)
+        cb(err)
+      } else {
+        process.env['API_TOKEN'] = data.Parameters[0].Value
+        process.env['DOMAIN'] = data.Parameters[1].Value
+        process.env['PASSWORD'] = data.Parameters[2].Value
+        cb(null, data)
+      }
+    })
   }
-})
+}
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-require('./env.js')
+variables = require('./env.js').variables
 const express = require('express')
 const app = express()
 
-console.log(process.env.API_TOKEN)
-console.log(process.env.PASSWORD)
-console.log(process.env.DOMAIN)
+variables((err, data) => {
+  if(err){
+    console.log(err)
+    process.exit(1)
+  }else{
+    apiToken = process.env.API_TOKEN
+    password = process.env.PASSWORD
+    domain = process.env.DOMAIN
+
+    app.listen(8080)
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('<html><h1>Secret Website</h1><br/><h3>API Token: '+apiToken+'</h3><h3>Password: '+password+'</h3><h3>Domain: '+domain+'</h3></html>')
+})
